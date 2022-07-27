4823dd6f9abb9ff931d90ce299905f30a19465e2
fflq <flqnerve@163.com>
2022-07-27 17:35:43 +0800

    add from ss

--- csispl/injection/run_inject.sh
+++ csispl/injection/run_inject.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+npac=20
+nintv=500000
+if [ $# -ge 1 ]; then
+	npac=$1 ;
+fi
+if [ $# -ge 2 ]; then
+	nintv=$2 ;
+fi
 
-echo 0x4101 | sudo tee /sys/kernel/debug/iwlwifi/0000:08:00.0/iwldvm/debug/monitor_tx_rate
+echo 0x4101 | sudo tee /sys/kernel/debug/iwlwifi/0000:03:00.0/iwldvm/debug/monitor_tx_rate
 
 #./random_packets 100 100 1 200000
-./random_packets 5000 20 1 300000
+./random_packets $npac 20 1 $nintv
 
