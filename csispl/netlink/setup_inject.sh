dd4bc26b9260884a6116b55cb840b6f93a23ea1c
fflq <flqnerve@163.com>
2022-07-27 17:35:43 +0800

    add from ss

--- csispl/netlink/setup_inject.sh
+++ csispl/netlink/setup_inject.sh
@@ -1,23 +1,39 @@
 #!/usr/bin/sudo /bin/bash
-set -x
-wlan=wlp8s0
+set -x ;
+
+chn=64
+bw=HT20
+if [ $# -ge 2 ]; then
+	chn=$1
+	bw=$2
+fi 
 
 modprobe -r iwlwifi mac80211 cfg80211
-#modprobe iwlwifi debug=0x40000
-modprobe iwlwifi connector_log=1 debug=0x40000
+#modprobe iwlwifi connector_log=0x1 debug=0x40000
+#0b100,for rx_mpdu; 0b1,for bfee_notif
+#modprobe iwlwifi connector_log=0x1 debug=0x40000
+modprobe iwlwifi debug=0x40000
 
 service network-manager stop
-ifconfig $wlan down
-ifconfig $wlan 2>/dev/null 1>/dev/null
+killall wpa_supplicant
+
+
+## inject
+inj_wlan=wlp3s0
+ifconfig $inj_wlan 2>/dev/null 1>/dev/null
 while [ $? -ne 0 ]
 do
-	        ifconfig $wlan 2>/dev/null 1>/dev/null
+	ifconfig $inj_wlan 2>/dev/null 1>/dev/null
+	sleep 1
 done
-iw dev $wlan interface add mon0 type monitor
-ifconfig $wlan up
-iw mon0 set channel $1 $2
-ifconfig mon0 up
+ifconfig $inj_wlan down
+iw dev $inj_wlan interface add inj0 type monitor
+#ifconfig $inj_wlan up
+ifconfig inj0 up
+iw inj0 set channel $chn $bw
 
 
 #service network-manager start
+
+
 
