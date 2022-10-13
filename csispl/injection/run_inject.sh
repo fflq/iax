0a549da38b3d7a55015591ea74923de08762980d
fflq <flqnerve@163.com>
2022-10-13 23:49:54 +0800

    save dbg change

--- csispl/injection/run_inject.sh
+++ csispl/injection/run_inject.sh
@@ -1,16 +1,21 @@
-#!/bin/bash
+#!/usr/bin/sudo /bin/bash
 
+wlan=inj0
 npac=20
 nintv=500000
 if [ $# -ge 1 ]; then
-	npac=$1 ;
+	wlan=$1 ;
 fi
 if [ $# -ge 2 ]; then
-	nintv=$2 ;
+	npac=$2 ;
+fi
+if [ $# -ge 3 ]; then
+	nintv=$3 ;
 fi
 
 echo 0x4101 | sudo tee /sys/kernel/debug/iwlwifi/0000:03:00.0/iwldvm/debug/monitor_tx_rate
+echo 0x4101 | sudo tee /sys/kernel/debug/iwlwifi/0000:07:00.0/iwldvm/debug/monitor_tx_rate
 
 #./random_packets 100 100 1 200000
-./random_packets $npac 20 1 $nintv
+./random_packets $npac 20 1 $nintv $wlan
 
--- csispl/netlink/set_injector_func.sh
+++ csispl/netlink/set_injector_func.sh
@@ -1,3 +1,4 @@
+set -x ;
 
 set_injector_func() {
 	if [ $# -lt 3 ]; then
@@ -5,15 +6,15 @@ set_injector_func() {
 		return ;
 	fi
 	echo "set $1 injector $2 $3" ;
-## inject
 	wlan=$1 
 	chn=$2
 	bw=$3
+
     ifconfig $wlan 2>/dev/null 1>/dev/null
     while [ $? -ne 0 ]
     do
-	    ifconfig $wlan 2>/dev/null 1>/dev/null
 	    sleep 1
+	    ifconfig $wlan 2>/dev/null 1>/dev/null
     done
     ifconfig $wlan down
     iw dev $wlan interface add inj0 type monitor
